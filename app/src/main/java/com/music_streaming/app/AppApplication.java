3e01aa35540c5d889a1106d5b87e58a1409e52bf
Nemanja <nemanja.nikolic98@hotmail.com>
2025-06-11 14:20:29 +0200

    init

---
+++ app/src/main/java/com/music_streaming/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.music_streaming.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
